ba309665a673738a29b97276f767ec8f43449d4a
ssoso27 <sohi8290@gmail.com>
2019-06-09 02:55:41 +0900

    [F8] 뉴스피드 페이지 추가
    - bootstrap 세팅
    - 뉴스피드 페이지 생성

---
+++ tlog/src/components/NewsFeed.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>NewsFeed</h1>
+</template>
+
+<script>
+export default {
+    name: "NewsFeed"
+}
+</script>
--- tlog/src/router/index.js
+++ tlog/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import NewsFeed from '@/components/NewsFeed'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'NewsFeed',
+      component: NewsFeed
     }
   ]
 })
