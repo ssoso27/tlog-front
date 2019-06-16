5c70b996e16c1d80b4348d8fd366bd9033c4447b
ssoso27 <sohi8290@gmail.com>
2019-06-17 06:24:03 +0900

    [F11] 여행기 보기 페이지 추가
    - 여행기 부분 추가

--- tlog/src/components/TLogDetail.vue
+++ tlog/src/components/TLogDetail.vue
@@ -1,24 +1,52 @@
 <template>
 <div class="container">
-  <div class="row">
-    <div class="header-container"
-        style="background: url(http://www.5viral.com/wp-content/uploads/2016/02/round-window-in-airplane.jpg);">
-        <p class="header-title">
-            <span class="text-background">
-                첫 여행기
-            </span>
-        </p>
-        <p class="header-period">
-            <span class="text-background">
-                2019.04.03 ~ 2019.04.06
-            </span>
-        </p>
-        <p>
-            <span class="header-hashtag text-background">#제주여행</span>
-            <span class="header-hashtag text-background">#친구들과</span>
-        </p>
-    </div>
-  </div>
+    <section>
+        <div class="row">
+            <div class="header-container"
+                style="background: url(http://www.5viral.com/wp-content/uploads/2016/02/round-window-in-airplane.jpg);">
+                <p class="header-title">
+                    <span class="text-background">
+                        첫 여행기
+                    </span>
+                </p>
+                <p class="header-period">
+                    <span class="text-background">
+                        2019.04.03 ~ 2019.04.06
+                    </span>
+                </p>
+                <p>
+                    <span class="header-hashtag text-background">#제주여행</span>
+                    <span class="header-hashtag text-background">#친구들과</span>
+                </p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="row mt-3">
+            <p class="h2 text-center col-12">4월 3일</p>
+        </div>
+        <div class="row">
+            <i class="fas fa-check-circle mr-2" style="font-size: 45px"></i>
+            <p>
+                <span class="h3">제주국제공항</span>
+                <span>12:00~</span>
+            </p>
+        </div>
+        <div class="row" style="padding: 5px">
+            <div class="col-12 row">
+                <img
+                    src="https://s-ec.bstatic.com/images/hotel/max1024x768/989/98999751.jpg"
+                    class="rounded border"
+                    alt="..."
+                    style="width:200px; height:200px">
+            </div>
+            <div class="col-12 row">
+                <p class="font-weight-normal mt-2">
+                    바다에 갈 기대중~
+                </p>
+            </div>
+        </div>
+    </section>
 </div>
 </template>
 
@@ -38,9 +66,9 @@ export default {
 
 .header-container > p.header-title {
     margin-top: 7%;
-	text-align: center;
-	font-weight: bold;
-	font-size: 30px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 30px;
 }
 
 .header-container > span.header-hashtag {
@@ -52,14 +80,14 @@ export default {
 }
 
 .header-container > p {
-	color: white;
+    color: white;
     margin-left: 10px;
     margin-right: 10px;
 }
 
 .text-background {
-	background-color: rgba(6,6,6,0.3);
-	text-shadow: 3px 3px 10px #000;
+    background-color: rgba(6,6,6,0.3);
+    text-shadow: 3px 3px 10px #000;
 }
 
 </style>
