9ea396936179b5914b6aa8a5c7c862a48745e094
ssoso27 <sohi8290@gmail.com>
2019-06-19 06:01:01 +0900

    [F10] 여행기 작성 페이지 추가
    - 장소 추가

--- tlog/src/components/InputPlace.vue
+++ tlog/src/components/InputPlace.vue
@@ -1,5 +1,27 @@
 <template>
-  
+  <div class="row">
+      <div class="row col-12">
+          <i class="fas fa-check-circle mr-2" style="font-size: 45px"></i>
+          <p>
+              <span class="h3">제주국제공항</span>
+              <span>12:00~</span>
+          </p>
+      </div>
+      <div class="row col-12" style="padding: 5px">
+          <div class="row col-12">
+              <img
+                  src="https://s-ec.bstatic.com/images/hotel/max1024x768/989/98999751.jpg"
+                  class="rounded border"
+                  alt="..."
+                  style="width:200px; height:200px">
+          </div>
+          <div class="row col-12">
+              <p class="font-weight-normal mt-2">
+                  바다에 갈 기대중~
+              </p>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
--- tlog/src/components/TLogContentWrite.vue
+++ tlog/src/components/TLogContentWrite.vue
@@ -5,29 +5,8 @@
             <label for="date" class="lab">날짜</label>
             <datepicker v-model="date" :format="format" placeholder="select date"></datepicker>
         </div>
-        <div class="row">
-            <div class="row col-12">
-                <i class="fas fa-check-circle mr-2" style="font-size: 45px"></i>
-                <p>
-                    <span class="h3">제주국제공항</span>
-                    <span>12:00~</span>
-                </p>
-            </div>
-            <div class="row col-12" style="padding: 5px">
-                <div class="row col-12">
-                    <img
-                        src="https://s-ec.bstatic.com/images/hotel/max1024x768/989/98999751.jpg"
-                        class="rounded border"
-                        alt="..."
-                        style="width:200px; height:200px">
-                </div>
-                <div class="row col-12">
-                    <p class="font-weight-normal mt-2">
-                        바다에 갈 기대중~
-                    </p>
-                </div>
-            </div>
-        </div>
+        <component v-for="(input_place, idx) in input_places" :is="input_place" v-bind:key="input_place.id"></component>
+        <button type="button" class="btn btn-primary" v-on:click="addPlace">다른 장소 추가</button>
     </form>
 </div>
 </template>
@@ -40,13 +19,21 @@ export default {
     name: 'ContentWrite',
     components: {
         Datepicker,
-        InputPlace
+        'input_place': InputPlace
     },
     data () {
         return {
             format: 'MM월 dd일',
             date: new Date(),
             input_places: []
+        }
+    },
+    methods: {
+        addPlace: function (event) {
+            this.input_places.push('input_place')
+        },
+        submit: function () {
+            alert('submit')
         }
     }
 }
