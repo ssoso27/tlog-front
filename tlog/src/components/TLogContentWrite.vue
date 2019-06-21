671ec334609f0c71135d0c834d163a7389fc95e1
ssoso27 <sohi8290@gmail.com>
2019-06-21 12:55:52 +0900

    [F28] TDate 작성 기능 연결
    - tdate 조회 기능과 연결

--- tlog/src/components/TLogContentWrite.vue
+++ tlog/src/components/TLogContentWrite.vue
@@ -1,17 +1,15 @@
 <template>
 <div class="container">
-    <form style="width:100%" @submit.prevent="submit">
-        <div class="row mt-3 justify-content-center">
-            <p class="h2 text-center col-12">{{ tdate.date }}</p>
-        </div>
-        <component v-for="(input_place, idx) in input_places" :is="input_place" v-bind:key="input_place.id" v-model="places[idx]" :seq="idx"></component>
-        <div class="row">
-            <button type="button" class="btn btn-primary mt-2" v-on:click="addPlace">다른 장소 추가</button>
-        </div>
-        <div class="row">
-            <button type="submit" class="btn btn-outline-dark mt-2">다음 날짜</button>
-        </div>
-    </form>
+    <div class="row mt-3 justify-content-center">
+        <p class="h2 text-center col-12">{{ tdate.date }}</p>
+    </div>
+    <component v-for="(input_place, idx) in input_places" :is="input_place" v-bind:key="input_place.id" v-model="places[idx]" :seq="idx"></component>
+    <div class="row">
+        <button type="button" class="btn btn-primary mt-2" v-on:click="addPlace">다른 장소 추가</button>
+    </div>
+    <div class="row">
+        <button type="button" class="btn btn-outline-dark mt-2">다음 날짜</button>
+    </div>
 </div>
 </template>
 
@@ -37,12 +35,13 @@ export default {
         this.tdate_id = this.$route.params.tdateId
         this.input_places = []
         this.places = []
+        this.getTdate()
     },
     methods: {
         getTdate: function () {
             this.$axios.get('/api/tdate/' + this.tdate_id)
             .then((response) => {
-                this.date = response.data
+                this.tdate = response.data
             })
             .catch(function () {
                 alert('해당 날짜의 기록을 가져올 수 없습니다.')
@@ -50,14 +49,6 @@ export default {
         },
         addPlace: function (event) {
             this.input_places.push('input_place')
-        },
-        submit: function () {
-            // var today = new Date(this.date).format('yyyy-MM-dd')
-            alert('submit')
-            // alert(today)
-            console.log(this.date)
-            console.log(this.places)
-            // this.$router.push('/tlog/write/1/date/2019-03-02')
         }
     }
 }
